6b2aba9590b26dd3aa375587cb518079c12b7871
Sivasish Praharaj <sivasish48@gmail.com>
2025-02-17 23:58:56 +0530

    added starters and tailwind v4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center">
+        <img src="/logo.png" className="w-32 h-32" alt="logo" />
+        <h1 className="text-3xl font-bold">
+          Welcome to the <span className="text-red-500">Vite + React</span> template!
+        </h1>
+        <p className="text-gray-500">
+          This is a <span className="text-red-500">Vite + React</span> template. To get started, you can create a project using this template by running <code className="text-green-500">npm init vite@latest</code> or <code className="text-green-500">yarn create vite@latest</code>.
         </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
